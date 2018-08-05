fda44b3f0864fdfe204e6cac219500dad7829566
wu-xuemin <xuemin.wu@toshiba-tih.com>
2018-08-05 17:08:56 +0800

    在/repair/actual/info/add(repairActualInfo，partsInfo) 上传实际维修情况时，也同时上传了要寄回的配件

--- src/main/java/com/eservice/api/web/RepairActualInfoController.java
+++ src/main/java/com/eservice/api/web/RepairActualInfoController.java
@@ -1,10 +1,17 @@
 package com.eservice.api.web;
 import com.eservice.api.core.Result;
 import com.eservice.api.core.ResultGenerator;
+import com.eservice.api.model.parts_info.PartsInfo;
 import com.eservice.api.model.repair_actual_info.RepairActualInfo;
+import com.eservice.api.model.repair_record.RepairRecord;
+import com.eservice.api.service.PartsInfoService;
 import com.eservice.api.service.RepairActualInfoService;
+import com.eservice.api.service.common.Constant;
+import com.eservice.api.service.impl.RepairRecordServiceImpl;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,10 +32,26 @@ import java.util.List;
 public class RepairActualInfoController {
     @Resource
     private RepairActualInfoService repairActualInfoService;
+    @Resource
+    PartsInfoService partsInfoService;
+    @Resource
+    private RepairRecordServiceImpl repairRecordService;
 
+    /**
+     * 在上传实际维修情况时，也同时上传了要寄回的配件，更新record状态
+     */
     @PostMapping("/add")
-    public Result add(@RequestBody @NotNull RepairActualInfo repairActualInfo) {
-        repairActualInfoService.save(repairActualInfo);
+    public Result add(@RequestBody @NotNull RepairActualInfo repairActualInfo, @RequestBody @NotNull  PartsInfo partsInfo) {
+        try {
+            repairActualInfoService.save(repairActualInfo);
+            partsInfoService.update(partsInfo);
+            RepairRecord repairRecord = repairRecordService.findById(repairActualInfo.getRepairRecordId());
+            repairRecord.setStatus(Constant.REPAIR_STATUS_REPAIR_OK);
+            repairRecordService.update(repairRecord);
+        } catch (Exception ex) {
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            return ResultGenerator.genFailResult("数据更新出错！" + ex.getMessage());
+        }
         return ResultGenerator.genSuccessResult();
     }
 
