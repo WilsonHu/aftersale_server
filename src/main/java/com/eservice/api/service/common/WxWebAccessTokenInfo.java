1be7596f7ad1d576348f3c5f7cb386e6b1512040
wu-xuemin <wuxuemin2000@126.com>
2019-02-02 17:03:10 +0800

    step2. 文件改名wxWebAccessTokenInfo改为 大写开头（需要先删除再增加，才能被git识别到）

---
+++ src/main/java/com/eservice/api/service/common/WxWebAccessTokenInfo.java
@@ -0,0 +1,26 @@
+package com.eservice.api.service.common;
+
+/**
+ * 网页授权接口调用凭证
+ */
+public class WxWebAccessTokenInfo {
+
+    private String access_token;
+    private String openid;
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+}
